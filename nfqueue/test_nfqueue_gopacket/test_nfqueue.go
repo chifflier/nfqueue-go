4bed376296a2d8d8b3f347a2b074a8034a0ca731
Norwin Schnyder <nos@open.ch>
2017-04-25 09:39:03 +0200

    Move logging to examples

--- nfqueue/test_nfqueue_gopacket/test_nfqueue.go
+++ nfqueue/test_nfqueue_gopacket/test_nfqueue.go
@@ -31,7 +31,8 @@ func real_callback(payload *nfqueue.Payload) error {
         fmt.Println(gopacket.LayerDump(layer))
     }
     fmt.Println("-- ")
-    payload.SetVerdict(nfqueue.NF_ACCEPT)
+    fmt.Printf("Setting verdict for packet %d: %d\n", payload.Id, nfqueue.NF_ACCEPT)
+	payload.SetVerdict(nfqueue.NF_ACCEPT)
     return nil
 }
 
@@ -40,12 +41,25 @@ func main() {
 
     q.SetCallback(real_callback)
 
-    q.Init()
+    fmt.Println("Opening queue")
+    if err := q.Init(); err != nil {
+        fmt.Println(err.Error())
+    }
+    defer q.Close()
 
+    fmt.Println("Unbinding to selected family")
     q.Unbind(syscall.AF_INET)
-    q.Bind(syscall.AF_INET)
 
-    q.CreateQueue(0)
+    fmt.Println("Binding to selected family")
+    if err := q.Bind(syscall.AF_INET); err != nil {
+        fmt.Println(err.Error())
+    }
+
+    fmt.Println("Creating queue")
+    if err := q.CreateQueue(0); err != nil {
+        fmt.Println(err.Error())
+    }
+    q.SetMode(nfqueue.NFQNL_COPY_PACKET)
 
     c := make(chan os.Signal, 1)
     signal.Notify(c, os.Interrupt)
@@ -53,14 +67,23 @@ func main() {
         for sig := range c {
             // sig is a ^C, handle it
             _ = sig
+            fmt.Println("Stop Loop")
             q.StopLoop()
         }
     }()
 
     // XXX Drop privileges here
 
-    q.Loop()
-    q.DestroyQueue()
+    fmt.Println("Start Loop")
+    if err := q.Loop(); err != nil {
+        fmt.Println(err.Error())
+    }
+
+    fmt.Println("Destroy queue")
+    if err := q.DestroyQueue(); err != nil {
+        fmt.Println(err.Error())
+    }
+
     q.Close()
     os.Exit(0)
 }
